518f9f5704abfb3246d839739b156424e3599713
Michelle Lowe <mick_571@yahoo.com>
2018-03-06 19:37:55 -0800

    Estimate nth prime number and tests

--- src/main/groovy/PrimeTable.groovy
+++ src/main/groovy/PrimeTable.groovy
@@ -1,14 +1,36 @@
 class PrimeTable {
 
-  /* Use Sieve of Eratosthenes to find the prime numbers up to a given integer.
-  *  https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+  static void main(String[] args) {
+    int numOfPrimes = args*.toInteger()[0] ?: 10
+    int upperBound = estimateUpperBound(numOfPrimes)
+    try {
+      println getListOfPrimes(upperBound).take(numOfPrimes)
+    } catch (Exception e) {
+      println e.message
+    }
+  }
+
+  /*
+  * Use Prime number theorem to estimate the nth prime number
+  * https://en.wikipedia.org/wiki/Prime_number_theorem#Approximations_for_the_nth_prime_number
+  */
+  static int estimateUpperBound(int num) {
+    if (num < 6) {
+      return (num * 2) + 1
+    } else {
+      return Math.ceil(num * (Math.log(num) + Math.log(Math.log(num))))
+    }
+  }
+
+  /*
+  * Use Sieve of Eratosthenes to find the prime numbers up to a given integer.
+  * https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
   *
-  *  Given a list of numbers from 2 to a given integer(bound), assume all numbers are unmarked/prime.
-  *  Iterate through the list and unmark/eliminate multiples of each prime, starting with the first prime number, 2.
+  * Given a list of numbers from 2 to a given integer(bound), assume all numbers are unmarked/prime.
+  * Iterate through the list and unmark/eliminate multiples of each prime, starting with the first prime number, 2.
   *
-  *  Notice we do not need to check every number in the list, only up to the square root of the bound. The square root
-  *  is the smallest divisor, and any number larger than the square root is not a divisor of the bound.
-  *
+  * Notice we do not need to check every number in the list, only up to the square root of the bound. The square root
+  * is the smallest divisor, and any number larger than the square root is not a divisor of the bound.
   */
   static List<Integer> getListOfPrimes(Integer bound) {
     if (bound < 2) {
@@ -22,23 +44,14 @@ class PrimeTable {
     if (bound > 3) {
       (2..((int) Math.sqrt(bound))).each { n ->
         // Skip to the square of n. All numbers in between have already been checked by previous iterations and unmarked or left as marked
-        ((n**2)..bound).step(n) {
-          numList[it] = 0
+        ((n**2)..bound).step(n) { m ->
+          numList[m] = 0
         }
       }
     }
 
     // Unmarked/non-prime indices have been set to 0
     numList.findAll { it > 1 }
-  }
-
-  static void main(String[] args) {
-    int bound = args*.toInteger()[0] ?: 16
-    try {
-      println getListOfPrimes(bound)
-    } catch (Exception e) {
-      println e.message
-    }
   }
 
 }
--- src/test/groovy/PrimeTableTests.groovy
+++ src/test/groovy/PrimeTableTests.groovy
@@ -1,6 +1,5 @@
 import org.junit.Before
 import org.junit.Test
-import org.junit.internal.runners.statements.ExpectException
 
 class PrimeTableTests extends GroovyTestCase {
 
@@ -11,6 +10,22 @@ class PrimeTableTests extends GroovyTestCase {
     solution = new PrimeTable()
   }
 
+  /* estimateUpperBound */
+  @Test
+  void test_estimateUpperBound_NumOfPrimesLessThan6() {
+    int num = 3
+    assert solution.estimateUpperBound(num) == (2 * num) + 1
+  }
+
+  @Test
+  void test_estimateUpperBound_NumOfPrimesGreaterThan6() {
+    Map<Integer, Integer> nums = [10:32, 50:264, 100:614, 1000:8841, 10000:114307]
+    nums.each { k, v ->
+      assert solution.estimateUpperBound(k) == v
+    }
+  }
+
+  /* getListOfPrimes */
   @Test
   void test_getListOfPrimes_ReturnsList() {
     assert solution.getListOfPrimes(10) == [2, 3, 5, 7]
